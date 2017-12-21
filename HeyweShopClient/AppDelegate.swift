8bc9e1eed9e1f93bdea6ec7fd5bb84bf7926fcba
774655048@qq.com <774655048@qq.com>
2017-12-21 11:35:08 +0800

    first commit

--- HeyweShopClient/AppDelegate.swift
+++ HeyweShopClient/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let vc = UIViewController()
+        vc.view.backgroundColor = UIColor.red
+        
+        window?.rootViewController = vc
+        
         return true
     }
 
